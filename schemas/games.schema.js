67045ef68d3bf82f13fb6c1afeed4e3030b9928a
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-07-22 21:33:58 -0300

    refactor: add validate image to uri

--- schemas/games.schema.js
+++ schemas/games.schema.js
@@ -2,7 +2,7 @@ import joi from "joi";
 
 const gameSchema = joi.object({
     name: joi.string().required(),
-    image: joi.string().required(),
+    image: joi.string().uri().required(),
     stockTotal: joi.number().integer().min(1).required(),
     categoryId: joi.number().integer().min(1).required(),
     pricePerDay: joi.number().integer().min(0).required(),
