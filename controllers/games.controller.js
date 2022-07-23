b2fb98031d3d91088c80e7d806830f334a2374ea
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-07-22 22:00:45 -0300

    fix: search for game name

--- controllers/games.controller.js
+++ controllers/games.controller.js
@@ -2,11 +2,15 @@ import connection from "../databases/postgres.js";
 
 export async function listGames(req, res) {
   const { name } = req.query;
-  const queryName = name ? `WHERE games.name ILIKE '%${name}%'` : "";
+  const queryName = name ? `WHERE games.name ILIKE '${name}%'` : "";
 
   try {
     const { rows: games } = await connection.query(
-      `SELECT games.*, categories.name AS "categoryName" FROM games JOIN categories ON games."categoryId" = categories.id ${queryName}`
+      `SELECT games.*, categories.name AS "categoryName" 
+      FROM games 
+      JOIN categories 
+      ON games."categoryId" = categories.id 
+      ${queryName}`
     );
     res.send(games);
     return;
